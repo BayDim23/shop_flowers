3ae8e1454e314734d836e81564d6dd3189fd7a7d
BayDim23 <168500336+BayDim23@users.noreply.github.com>
2024-09-18 12:05:50 +0300

    1 add new code

--- flower_shop/flowers/urls.py
+++ flower_shop/flowers/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register'),
+    path('register', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('flower_catalog/', views.flower_catalog, name='flower_catalog'),
+    path('', views.flower_catalog, name='flower_catalog'),
     path('order/<int:flower_id>/', views.place_order, name='place_order'),
 ]
